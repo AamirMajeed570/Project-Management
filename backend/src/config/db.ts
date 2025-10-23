1e7c08cc2ce260c0936e3feae4684a9e0320c5bb
Aamir <aamirmajeedkhan570@gmail.com>
2025-10-23 11:00:47 +0530

    Fix

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,7 +3,7 @@ const MONGO_URI = process.env.MONGO_URI;
 // console.log("MONGO_URI:", MONGO_URI);
 export const connectDB = async (MONGO_URI: string) => {
     try {
-        await mongoose.connect("mongodb+srv://aamirmajeedkhan570_db_user:Fb4CtLYv9d8n6V3F@cluster0.4tdncf2.mongodb.net/Project-Management?retryWrites=true&w=majority&appName=Cluster0").then(() => {
+        await mongoose.connect(MONGO_URI).then(() => {
             console.log("MongoDB connected successfully");
         }).catch((err) => {
             console.error("MongoDB connection error:", err);
