3f36ed18c4ed28172d694fb51b785feb00f14f20
Aamir <aamirmajeedkhan570@gmail.com>
2025-10-24 23:17:11 +0530

    Fixed

--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 import app from "./src/app";
 dotenv.config();
 console.log(process.env.PORT);
-const PORT:any = process.env.PORT || 3000;
+const PORT:any = process.env.PORT || 8000;
 
-app.listen(PORT,()=> console.log(`Server listening at http://localhost:${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${process.env.PORT || 8000}`);
+});
